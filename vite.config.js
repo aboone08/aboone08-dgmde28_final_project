38f5db696b30059c271f0616493264a6ab40ae91
aboone08 <16787933+aboone08@users.noreply.github.com>
2025-06-02 22:46:00 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173, 
-  },
+  root: '.', // Ensures Vite looks in the right place
   build: {
-    outDir: 'dist',
+    outDir: 'dist'
   }
 });
